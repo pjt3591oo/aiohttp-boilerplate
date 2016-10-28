27c4f3e6f07973488d5dcd3e77fa56813c809fef
pjt3591oo <pjt3591oo@gmail.com>
2016-10-28 10:27:49 +0900

    라우팅 추가

--- routes.py
+++ routes.py
@@ -4,19 +4,33 @@ import asyncio
 
 
 def setup_route(app):
-    app.router.add_get('/responseText/{userKey}/{productKey}', responseText)
-    app.router.add_get('/test', test)
-
-
-def test(rq):
-    return web.Response(text = 'size')
+    app.router.add_get('/response/text/{u}/{p}', responseText)
+    app.router.add_get('/response/body/{u}/{p}', responseBody)
+    app.router.add_get('/response/json', responseJson)
+    app.router.add_get('/r', redirect)
 
 
 @asyncio.coroutine
 def responseText(rq):
-    userKey =  rq.match_info.get("userKey") or 'x'
-    productKey =  rq.match_info.get("productKey") or 'x'
+    userKey =  rq.match_info.get("u") or 'x'
+    productKey =  rq.match_info.get("p") or 'x'
 
     size = str(test_module(userInfo, productInfo))
 
     return  web.Response(text= size)
+
+
+@asyncio.coroutine
+def responseBody(rq):
+    return web.Response(body=b"Hello, world")
+
+
+@asyncio.coroutine
+def responseJson(rq):
+    data = {'some': 'data'}
+    return web.json_response(data)
+
+
+@asyncio.coroutine
+def redirect(request):
+    return web.HTTPFound('/response/json')
